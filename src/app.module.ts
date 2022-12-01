eb439a4514bdeb6314fdd21a106faac2ebcc7d7a
luanderrson <luandersonbms@gmail.com>
2022-12-01 15:31:26 -0300

    Troca da URL do mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,8 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://backluan:cebola083@cluster0.ij6yevh.mongodb.net/test',
+      //'mongodb+srv://backluan:cebola083@cluster0.ij6yevh.mongodb.net/test',
+      'mongodb://mongo:Np6g3Gw0ATn3CgqVftVt@containers-us-west-122.railway.app:7327',
     ),
     UsersModule,
   ],
